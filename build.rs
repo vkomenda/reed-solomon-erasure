b892c33bd1e00c2d7d928f3e487bd1ab94c59f9c
Vladimir Komendantskiy <komendantsky@gmail.com>
2020-05-23 19:01:13 +0100

    added conditional compilation for cross-compiling for Aarch64

--- build.rs
+++ build.rs
@@ -153,7 +153,10 @@ fn write_tables() {
 
 #[cfg(all(
     feature = "simd-accel",
-    any(target_arch = "x86_64", target_arch = "aarch64"),
+    any(
+        all(host_arch = "x86_64", target_arch = "x86_64"),
+        all(host_arch = "aarch64", target_arch = "aarch64")
+    ),
     not(target_env = "msvc"),
     not(any(target_os = "android", target_os = "ios"))
 ))]
@@ -161,6 +164,21 @@ fn compile_simd_c() {
     cc::Build::new()
         .opt_level(3)
         .flag("-march=native")
+        .flag("-std=c11")
+        .file("simd_c/reedsolomon.c")
+        .compile("reedsolomon");
+}
+
+#[cfg(all(
+    feature = "simd-accel",
+    any(
+        all(not(host_arch = "x86_64"), target_arch = "x86_64"),
+        all(not(host_arch = "aarch64"), target_arch = "aarch64")
+    )
+))]
+fn compile_simd_c() {
+    cc::Build::new()
+        .opt_level(3)
         .flag("-std=c11")
         .file("simd_c/reedsolomon.c")
         .compile("reedsolomon");
